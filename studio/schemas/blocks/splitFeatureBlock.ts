1313b7ad7657d24143a3701bc31b997519c13cde
Megan Hackman <meganhackman@Megans-MacBook-Pro.local>
2026-07-02 13:42:48 -0400

    Revert "Merge pull request #125 from FoundSearchMarketing/develop"
    This reverts commit e0866c690ba8e247516a3af1cfd818f18ed51967, reversing
    changes made to 8899b34ae219e56d14cd80ea6e496fe81d3cd44a.

--- studio/schemas/blocks/splitFeatureBlock.ts
+++ studio/schemas/blocks/splitFeatureBlock.ts
@@ -17,12 +17,6 @@ export default defineType({
     ctaField(),
     ...mediaFields,
     defineField({
-      name: 'hubspotFormId',
-      title: 'HubSpot Form ID',
-      type: 'string',
-      description: 'Embed a HubSpot form in the media slot when no image is set',
-    }),
-    defineField({
       name: 'imagePosition',
       title: 'Image Position',
       type: 'string',
@@ -33,18 +27,6 @@ export default defineType({
         ],
       },
       initialValue: 'left',
-    }),
-    defineField({
-      name: 'mediaHeight',
-      title: 'Media Height',
-      type: 'string',
-      options: {
-        list: [
-          { title: 'Standard', value: 'standard' },
-          { title: 'Short', value: 'short' },
-        ],
-      },
-      initialValue: 'standard',
     }),
     themeField('theme', 'Theme', 'light'),
   ],
